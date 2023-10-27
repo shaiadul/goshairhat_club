8201a5dd12b4761ea6cbd9f7a017463acbc9addb
Shaiadul Bashar <96824801+shaiadul@users.noreply.github.com>
2023-10-27 21:56:04 +0600

    name boost ball

--- components/banner/index.jsx
+++ components/banner/index.jsx
@@ -3,19 +3,32 @@ import React from "react";
 
 export const Banner = () => {
   return (
-    <section className="banner_container bg-red-100">
-      <div className="container mx-auto flex flex-col items-center px-5 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-5xl">
-        <span className="text-teal-400 text-3xl md:text-5xl font-bold mt-20">
-          ইদিলপুর পৌরসভা ফুটবল ক্লাব
+    <section className="my-10 relative">
+      <img
+        src="https://m.media-amazon.com/images/I/71rvaUfzByL._AC_SL1500_.jpg"
+        alt="banner"
+        className="w-80 h-80 object-cover absolute top-16 left-10 md:top-20 md:left-1/2 animate-bounce animate-spin duration-700 -z-10"
+      />
+      <div className="container mx-auto flex flex-col items-center px-5 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-5xl ">
+        <span className="text-teal-400 text-3xl md:text-4xl font-bold mt-20">
+          ডিফেন্ডিং স্কোয়াড অব গোসাইরহাট
         </span>
         <p className="px-8 mt-8 mb-12 text-lg font-semibold">
-        চ্যাম্পিয়ন তারা নয় যারা একবার খেলার মাঠে জয়ী হয়েই থেমে যায়। বরং তারাই প্রকৃত চ্যাম্পিয়ন যারা হারের পর হার সহ্য করে শুধুমাত্র খেলায় জেতার জন্য
+          চ্যাম্পিয়ন তারা নয় যারা একবার খেলার মাঠে জয়ী হয়েই থেমে যায়। বরং তারাই
+          প্রকৃত চ্যাম্পিয়ন যারা হারের পর হার সহ্য করে শুধুমাত্র খেলায় জেতার
+          জন্য
         </p>
         <div className="flex flex-wrap justify-center">
-          <Link href="/donation" className="px-8 py-3 m-2 text-sm md:text-lg font-semibold rounded bg-teal-400 text-white hover:opacity-80 duration-700">
+          <Link
+            href="/donation"
+            className="px-8 py-3 m-2 text-sm md:text-lg font-semibold rounded bg-teal-400 text-white hover:opacity-80 duration-700"
+          >
             Donation
           </Link>
-          <Link href="/hireplayer" className="px-8 py-3 m-2 text-sm md:text-lg font-semibold border-2 rounded text-teal-400 hover:text-white border-teal-400 hover:bg-teal-400 duration-700">
+          <Link
+            href="/hireplayer"
+            className="px-8 py-3 m-2 text-sm md:text-lg font-semibold border-2 rounded text-teal-400 hover:text-white border-teal-400 hover:bg-teal-400 duration-700"
+          >
             Hire Player
           </Link>
         </div>

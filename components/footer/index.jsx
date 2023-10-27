3179210ce597dbf85c6d6dfb439db17089d4d40e
Shaiadul Bashar <96824801+shaiadul@users.noreply.github.com>
2023-10-27 14:00:54 +0600

    my link add

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
         <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
           © 2023 Official —
           <a
-            href="https://twitter.com/knyttneve"
+            href="https://www.facebook.com/profile.php?id=100075003758713"
             class="text-gray-600 ml-1"
             rel="noopener noreferrer"
             target="_blank"
